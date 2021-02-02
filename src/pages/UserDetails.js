d75bcca09890a1d44fc715b96ae278f6d5dd124f
petrame <63057558+petrame@users.noreply.github.com>
2021-02-02 22:53:52 +0100

    redirect user after login + logout function

--- src/pages/UserDetails.js
+++ src/pages/UserDetails.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import styled from "styled-components/macro";
+
+import { users } from "../reducers/users";
 
 export const UserDetails = () => {
-    return (
-        <p>This is the user details page.</p>
-    )
-}
+  const user = useSelector((store) => store.users.user);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  if (!user.accessToken) {
+    history.push("/");
+  }
+
+  const handleOnClick = () => {
+    dispatch(users.actions.logOut());
+  };
+
+  return (
+    <>
+      <p>This is the user details page.</p>
+      <p>Hello {user.firstName}!</p>
+      <Button onClick={handleOnClick}>Log out</Button>
+    </>
+  );
+};
+
+const Button = styled.button`
+  cursor: pointer;
+  margin: 10px;
+  background: #29354b;
+  padding: 5px 35px 7px 35px;
+  border-radius: 25px;
+  font-family: "Open Sans", sans-serif;
+  font-size: 16px;
+  color: #fff;
+  outline: none;
+`;
