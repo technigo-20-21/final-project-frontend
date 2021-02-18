b013ae0999835c8c585b8b5a597ded8c535e90ae
Evelina Berg <evelina.berg@outlook.com>
2021-02-18 18:31:27 +0100

    Toggle heart works

--- src/components/FavouriteButton.js
+++ src/components/FavouriteButton.js
@@ -3,8 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { users } from "../reducers/users";
 import { FavouriteHeartButton } from "../library/ThumbStyles";
-import { FavouriteOutlinedButton } from "../library/FavoriteOutlinedButton";
-import { FavoriteFilledButton } from "../library/FavouriteFilledButton";
+import { FavouriteOutlinedButton } from "../library/FavouriteOutlinedButton";
+import { FavouriteFilledButton } from "../library/FavouriteFilledButton";
 
 export const FavouriteButton = ({ localId }) => {
   const dispatch = useDispatch();
@@ -12,18 +12,16 @@ export const FavouriteButton = ({ localId }) => {
   const [favourites, setFavourites] = useState(
     userFavourites ? userFavourites : []
   );
-  const [isFavourite, setIsFavourite] = useState(false);
-  console.log(favourites);
+  const [isFavourite, setIsFavourite] = useState(favourites ? true : false);
   const favouritesArray = favourites;
-  // console.log(isFavourite);
-  // console.log(favouritesArray);
-  // useEffect(() => {
-  //   // if (favouritesArray.includes(_id)) setIsFavourite(true);
-  // }, []);
+ 
+  useEffect(() => {
+    setFavourites(userFavourites);
+  }, [userFavourites]);
 
   const handleOnClick = () => {
     dispatch(users.actions.updateFavourites(localId));
-
+ 
     if (favouritesArray.includes(localId)) {
       setIsFavourite(false);
     } else {
@@ -34,7 +32,7 @@ export const FavouriteButton = ({ localId }) => {
   return (
     <FavouriteHeartButton onClick={handleOnClick}>
       {favouritesArray.includes(localId) ? (
-        <FavoriteFilledButton />
+        <FavouriteFilledButton />
       ) : (
         <FavouriteOutlinedButton />
       )}
